882e82df86c90b72a878ad1e641c2322aea516fc
cygnu <crucia8@icloud.com>
2021-08-13 03:38:42 +0900

    refactor: Implement apollo client. Referring to https://www.apollographql.com/docs/react/networking/authentication/#header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://127.0.0.1:8000/graphql/',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token')
+  // return the headers to the contexts so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : "",
+    },
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 export const App: React.FC = () => {
   return (
-    <div></div>
+    <ApolloProvider client={client}>
+      <div></div>
+    </ApolloProvider>
   );
-}
+};
