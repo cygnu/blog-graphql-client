7b6f4532e6f3bf0fc4708f331f2d3ae1d306ff0b
cygnu <crucia8@icloud.com>
2021-08-13 12:52:15 +0900

    refactor: Implement components/Sidebar.tsx

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  Grid
+} from '@material-ui/core';
+
+export const Sidebar: React.FC = () => {
+  return (
+    <Grid item xs={12} md={4}>
+      <h1>Sidebar</h1>
+    </Grid>
+  );
+};
