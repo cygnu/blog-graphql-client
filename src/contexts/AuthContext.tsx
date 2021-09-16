53a6377a616d56b50b3253741f9ce640c804b147
cygnu <crucia8@icloud.com>
2021-09-16 11:53:03 +0900

    feat: Use 'useEffect' in contexts/AuthContext.tsx to save the token to local storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,9 @@
 import React, {
   createContext,
+  useEffect,
   useState,
 } from 'react';
+import jwtDecode from 'jwt-decode';
 import {
   User,
   IContext,
@@ -11,6 +13,18 @@ const AuthContext = createContext<IContext>({} as IContext);
 
 export const AuthProvider: React.FC = (props: any) => {
   const [user, setUser] = useState<User | null | undefined>(undefined);
+
+  useEffect(() => {
+    const storageItem = localStorage.getItem("token")
+    if (storageItem) {
+      const jwtDecodedToken = jwtDecode<User>(storageItem)
+      if (jwtDecodedToken.exp * 1000 < Date.now()) {
+        localStorage.removeItem("token")
+      } else {
+        setUser(jwtDecodedToken)
+      }
+    }
+  }, [])
 
   return (
     <AuthContext.Provider
