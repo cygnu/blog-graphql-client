11cd6ed0bde4d019eabe9c933de19f9d66fe32aa
cygnu <crucia8@icloud.com>
2021-09-19 01:22:12 +0900

    feat: Remove '@ts-ignore' and define types in contexts/PostContext.tsx

--- src/contexts/PostContext.tsx
+++ src/contexts/PostContext.tsx
@@ -1,11 +1,34 @@
 import React, {
   createContext,
 } from 'react';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { GET_POST } from '../graphql/queries';
 
-// @ts-ignore
-const PostContext = createContext();
+interface IPost {
+  getPost: any;
+  dataPost: any | undefined;
+  errorPost: any | undefined;
+}
 
-// @ts-ignore
-const PostProvider: React.FC = () => {
-  return
+const PostContext = createContext<IPost>({} as IPost);
+
+const PostProvider: React.FC = (props: any) => {
+  const [
+    getPost,
+    { data: dataPost, error: errorPost }
+  ] = useLazyQuery(GET_POST, { fetchPolicy: "network-only" });
+
+  return (
+    <PostContext.Provider
+      value={{
+        getPost,
+        dataPost,
+        errorPost,
+      }}
+    >
+      {props.children}
+    </PostContext.Provider>
+  );
 };
+
+export { PostContext, PostProvider };
