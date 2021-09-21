05fedc2f1176b606d3b87ba60d8ea259bfde4a71
cygnu <crucia8@icloud.com>
2021-09-22 00:39:14 +0900

    refactor: Update pages/MergeProfile.tsx

---
+++ src/pages/MergeProfile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+
+export const MergeProfile: React.FC = () => {
+  return (
+    <Container>
+      <form></form>
+    </Container>
+  );
+};
