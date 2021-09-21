24a6d1c082b8511c840dbbeef23b90d5fa080d6d
cygnu <crucia8@icloud.com>
2021-09-22 00:53:36 +0900

    refactor: Add basic validations in pages/MergeProfile.tsx

--- src/pages/MergeProfile.tsx
+++ src/pages/MergeProfile.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import * as Yup from 'yup';
 import { Container } from '@material-ui/core';
+
+const schema = Yup.object().shape({
+  username: Yup.string()
+    .required(),
+  first_name: Yup.string(),
+  last_name: Yup.string(),
+  avatar: Yup.string(),
+  background_image: Yup.string(),
+  local: Yup.string(),
+  bio: Yup.string(),
+  github_url: Yup.string()
+    .required(),
+  qiita_url: Yup.string(),
+  twitter_url: Yup.string(),
+  website_url: Yup.string(),
+});
 
 export const MergeProfile: React.FC = () => {
   return (
