28db8dada9fe47aa51e72ac4ce451f1ecb3389dc
cygnu <crucia8@icloud.com>
2021-09-22 00:02:20 +0900

    feat: Remove '@ts-ignore' and define types in contexts/ViewerContext.tsx

--- src/contexts/ViewerContext.tsx
+++ src/contexts/ViewerContext.tsx
@@ -1,11 +1,38 @@
 import React, {
-  createContext
+  createContext,
+  useContext,
 } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_VIEWER } from '../graphql/queries';
 
-// @ts-ignore
-const ViewerContext = createContext();
+interface IViewer {
+  loadingViewer: boolean;
+  errorViewer: any | undefined;
+  dataViewer: any | undefined;
+}
 
-// @ts-ignore
-const ViewerProvider: React.FC = () => {
-  return
+const ViewerContext = createContext({} as IViewer);
+
+const useViewer = () => useContext(ViewerContext);
+
+const ViewerProvider: React.FC = (props: any) => {
+  const {
+    loading: loadingViewer,
+    error: errorViewer,
+    data: dataViewer,
+  } = useQuery(GET_VIEWER);
+
+  return (
+    <ViewerContext.Provider
+      value={{
+        loadingViewer,
+        errorViewer,
+        dataViewer,
+      }}
+    >
+      {props.children}
+    </ViewerContext.Provider>
+  )
 };
+
+export { useViewer, ViewerProvider };
