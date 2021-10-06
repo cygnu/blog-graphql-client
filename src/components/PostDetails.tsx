ca153df6378c1071c5b90fa1e515157a7230a65d
cygnu <crucia8@icloud.com>
2021-10-07 02:56:12 +0900

    feat: Use 'useParams' in components/PostDetails.tsx

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export const PostDetails: React.FC = () => {
+  const { id } = useParams<{
+    id: string;
+  }>();
+
   return (
-    <div>PostDetails</div>
+    <h1>PostDetails ID: {id}</h1>
   );
 };
