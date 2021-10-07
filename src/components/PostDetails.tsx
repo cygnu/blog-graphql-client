f700b64ab9f06288fc5ddf4610841789a31bb0e9
cygnu <crucia8@icloud.com>
2021-10-08 03:56:10 +0900

    fix: Update components/PostDetails.tsx

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { PostContext } from '../contexts/PostContext';
 
 type PostDetailsProps = RouteComponentProps<{
   id: string;
 }>;
 
 export const PostDetails: React.FC<PostDetailsProps> = (props) => {
-  const id = props.match.params.id;
+  const { dataPost } = useContext(PostContext);
+  dataPost.post.id = props.match.params.id;
 
   return (
-    <h1>PostDetails ID: {id}</h1>
+    <h1>PostDetails ID: {dataPost.post.id}</h1>
   );
 };
