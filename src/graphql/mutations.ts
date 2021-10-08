71c2d760f8c5593b1a848088d43646c16c2bdc6d
cygnu <crucia8@icloud.com>
2021-10-08 16:33:01 +0900

    style: Code reformat in graphql/*.ts

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,38 +1,45 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const CREATE_USER = gql`
-  mutation($email: String!, $password: String!) {
-    createUser(input: {
-      email: $email,
-      password: $password
-    }) {
+  mutation ($email: String!, $password: String!) {
+    createUser(input: { email: $email, password: $password }) {
       user {
         id
         email
       }
     }
   }
-`
+`;
 
 export const GET_TOKEN = gql`
-  mutation($email: String!, $password: String!) {
+  mutation ($email: String!, $password: String!) {
     tokenAuth(email: $email, password: $password) {
       token
     }
   }
-`
+`;
 
 export const CREATE_PROFILE = gql`
-  mutation($username: String!, $firstName: String, $lastName: String, $avatar: String, $backgroundImage: String, $local: String, $bio: String) {
-    createProfile(input: {
-      username: $username,
-      firstName: $firstName,
-      lastName: $lastName,
-      avatar: $avatar,
-      backgroundImage: $backgroundImage,
-      local: $local,
-      bio: $bio
-    }) {
+  mutation (
+    $username: String!
+    $firstName: String
+    $lastName: String
+    $avatar: String
+    $backgroundImage: String
+    $local: String
+    $bio: String
+  ) {
+    createProfile(
+      input: {
+        username: $username
+        firstName: $firstName
+        lastName: $lastName
+        avatar: $avatar
+        backgroundImage: $backgroundImage
+        local: $local
+        bio: $bio
+      }
+    ) {
       user {
         id
         username
@@ -45,21 +52,31 @@ export const CREATE_PROFILE = gql`
       bio
     }
   }
-`
-
+`;
 
 export const UPDATE_PROFILE = gql`
-  mutation($id: Uuid!, $username: String!, $firstName: String, $lastName: String, $avatar: String, $backgroundImage: String, $local: String, $bio: String) {
-    createProfile(input: {
-      id: $id,
-      username: $username,
-      firstName: $firstName,
-      lastName: $lastName,
-      avatar: $avatar,
-      backgroundImage: $backgroundImage,
-      local: $local,
-      bio: $bio
-    }) {
+  mutation (
+    $id: Uuid!
+    $username: String!
+    $firstName: String
+    $lastName: String
+    $avatar: String
+    $backgroundImage: String
+    $local: String
+    $bio: String
+  ) {
+    createProfile(
+      input: {
+        id: $id
+        username: $username
+        firstName: $firstName
+        lastName: $lastName
+        avatar: $avatar
+        backgroundImage: $backgroundImage
+        local: $local
+        bio: $bio
+      }
+    ) {
       user {
         id
         username
@@ -72,16 +89,23 @@ export const UPDATE_PROFILE = gql`
       bio
     }
   }
-`
+`;
 
 export const CREATE_LINK_IN_BIO = gql`
-  mutation($githubUrl: String!, $qiitaUrl: String, $twitterUrl: String, $websiteUrl: String) {
-    createLinkInBio(input: {
-      githubUrl: $githubUrl,
-      qiitaUrl: $qiitaUrl,
-      twitterUrl: $twitterUrl,
-      websiteUrl: $websiteUrl
-    }) {
+  mutation (
+    $githubUrl: String!
+    $qiitaUrl: String
+    $twitterUrl: String
+    $websiteUrl: String
+  ) {
+    createLinkInBio(
+      input: {
+        githubUrl: $githubUrl
+        qiitaUrl: $qiitaUrl
+        twitterUrl: $twitterUrl
+        websiteUrl: $websiteUrl
+      }
+    ) {
       linkInBio {
         id
         githubUrl
@@ -91,17 +115,25 @@ export const CREATE_LINK_IN_BIO = gql`
       }
     }
   }
-`
+`;
 
 export const UPDATE_LINK_IN_BIO = gql`
-  mutation($id: Uuid!, $githubUrl: String, $qiitaUrl: String, $twitterUrl: String, $websiteUrl: String) {
-    createLinkInBio(input: {
-      id: $id
-      githubUrl: $githubUrl,
-      qiitaUrl: $qiitaUrl,
-      twitterUrl: $twitterUrl,
-      websiteUrl: $websiteUrl
-    }) {
+  mutation (
+    $id: Uuid!
+    $githubUrl: String
+    $qiitaUrl: String
+    $twitterUrl: String
+    $websiteUrl: String
+  ) {
+    createLinkInBio(
+      input: {
+        id: $id
+        githubUrl: $githubUrl
+        qiitaUrl: $qiitaUrl
+        twitterUrl: $twitterUrl
+        websiteUrl: $websiteUrl
+      }
+    ) {
       linkInBio {
         githubUrl
         qiitaUrl
@@ -110,10 +142,10 @@ export const UPDATE_LINK_IN_BIO = gql`
       }
     }
   }
-`
+`;
 
 export const ADD_TAG = gql`
-  mutation($name: String!) {
+  mutation ($name: String!) {
     addTag(input: { name: $name }) {
       tag {
         id
@@ -121,20 +153,20 @@ export const ADD_TAG = gql`
       }
     }
   }
-`
+`;
 
 export const REMOVE_TAG = gql`
-  mutation($id: Uuid!) {
+  mutation ($id: Uuid!) {
     removeTag(input: { id: $id }) {
       tag {
         id
       }
     }
   }
-`
+`;
 
 export const ADD_CATEGORY = gql`
-  mutation($name: String!) {
+  mutation ($name: String!) {
     addCategory(input: { name: $name }) {
       category {
         id
@@ -142,29 +174,39 @@ export const ADD_CATEGORY = gql`
       }
     }
   }
-`
+`;
 
 export const REMOVE_CATEGORY = gql`
-  mutation($id: Uuid!) {
+  mutation ($id: Uuid!) {
     removeCategory(input: { id: $id }) {
       category {
         id
       }
     }
   }
-`
+`;
 
 export const CREATE_POST = gql`
-  mutation($title: String!, $description: String, $thumbnail: thumbnail, $content: String!, $tags: [String], $category: String!, $isPublish: Boolean!) {
-    createPost(input: {
-      title: $title
-      description: $description
-      thumbnail: $thumbnail
-      content: $content
-      tags: $tags
-      category: $category
-      isPublish: $isPublish
-    }) {
+  mutation (
+    $title: String!
+    $description: String
+    $thumbnail: thumbnail
+    $content: String!
+    $tags: [String]
+    $category: String!
+    $isPublish: Boolean!
+  ) {
+    createPost(
+      input: {
+        title: $title
+        description: $description
+        thumbnail: $thumbnail
+        content: $content
+        tags: $tags
+        category: $category
+        isPublish: $isPublish
+      }
+    ) {
       post {
         id
         title
@@ -181,20 +223,31 @@ export const CREATE_POST = gql`
       }
     }
   }
-`
+`;
 
 export const UPDATE_POST = gql`
-  mutation($id: Uuid!, $title: String, $description: String, $thumbnail: thumbnail, $content: String, $tags: [Uuid], $category: String, $isPublish: Boolean!) {
-    createPost(input: {
-      id: $id
-      title: $title
-      description: $description
-      thumbnail: $thumbnail
-      content: $content
-      tags: $tags
-      category: $category
-      isPublish: $isPublish
-    }) {
+  mutation (
+    $id: Uuid!
+    $title: String
+    $description: String
+    $thumbnail: thumbnail
+    $content: String
+    $tags: [Uuid]
+    $category: String
+    $isPublish: Boolean!
+  ) {
+    createPost(
+      input: {
+        id: $id
+        title: $title
+        description: $description
+        thumbnail: $thumbnail
+        content: $content
+        tags: $tags
+        category: $category
+        isPublish: $isPublish
+      }
+    ) {
       post {
         id
         title
@@ -211,14 +264,15 @@ export const UPDATE_POST = gql`
       }
     }
   }
-`
+`;
 
 export const DELETE_POST = gql`
-  mutation($id: Uuid!) {
+  mutation ($id: Uuid!) {
     deletePost(input: { id: $id })
-  } {
+  }
+  {
     post {
       id
     }
   }
-`
+`;
