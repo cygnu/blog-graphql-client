964363f3d21458b67c87df371b15d4194295aa1c
cygnu <crucia8@icloud.com>
2021-10-10 02:46:26 +0900

    feat: Implement 'react-syntax-highlighter' in components/PostDetails.tsx

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -2,8 +2,35 @@ import React, { useContext } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { hopscotch } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { Container } from "@mui/material";
 import { PostContext } from "../contexts/PostContext";
+
+// @ts-ignore
+const MarkdownViewer: React.FC = ({ markdown }) => {
+  return (
+    <ReactMarkdown
+      children={markdown}
+      components={{
+        code({ node, className, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              style={hopscotch}
+              {...props}
+            />
+          ) : (
+            <code className={className} {...props} />
+          );
+        },
+      }}
+      remarkPlugins={[remarkGfm]}
+    />
+  );
+};
 
 type PostDetailsProps = RouteComponentProps<{
   id: string;
@@ -21,7 +48,7 @@ export const PostDetails: React.FC<PostDetailsProps> = (props) => {
       dataPost.post.id === props.match.params.id ? (
         <React.Fragment>
           <h1>PostDetails ID: {dataPost.post.id}</h1>
-          <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+          <MarkdownViewer children={markdown} />
         </React.Fragment>
       ) : (
         <React.Fragment />
