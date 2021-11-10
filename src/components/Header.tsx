db658eb8e8b3e885cea54dc01d2816cde2f0f2c3
cygnu <crucia8@icloud.com>
2021-11-11 04:25:12 +0900

    feat: If you are not logged in, you will see the login button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { useAuth } from "../contexts/AuthContext";
 
 export const Header: React.FC = () => {
+  const { currentUser } = useAuth();
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6">Title</Typography>
+        {currentUser ? (
+          <React.Fragment />
+        ) : (
+          <Button variant="outlined" size="small" component={Link} to="/auth">
+            Login
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
