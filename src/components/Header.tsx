03d15c11791ee030c5b2beb9895a8545ee0d82d7
cygnu <crucia8@icloud.com>
2021-11-11 14:35:16 +0900

    error: {dataViewer.viewer.user.email} is not displayed when logging in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { useViewer } from "../contexts/ViewerContext";
 import { useAuth } from "../pages/Authentication";
 
 export const Header: React.FC = () => {
+  const { dataViewer } = useViewer();
   const { currentUser } = useAuth();
 
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6">Title</Typography>
-        {currentUser ? (
-          <React.Fragment />
+        {currentUser?.token ? (
+          <p>{dataViewer.viewer.user.email}</p>
         ) : (
-          <Button variant="outlined" size="small" component={Link} to="/auth">
+          <Button variant="outlined" size="small" component={Link} to="/auth" color="inherit">
             Login
           </Button>
         )}
