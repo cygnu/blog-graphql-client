7040e30ea92b1976659f3cff85ed141b449b4097
cygnu <crucia8@icloud.com>
2021-11-13 13:46:48 +0900

    fix: Error handling for ` e861854 `

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,12 @@ export const Main: React.FC = () => {
   const { loadingViewer, errorViewer } = useViewer();
 
   if (loadingPosts || loadingViewer) return <h1>Loading...</h1>;
-  if (errorPosts || errorViewer) return <h1>{errorPosts.message || errorViewer.message}</h1>;
+  if (errorPosts || errorViewer) return (
+    <React.Fragment>
+      <h1>{errorPosts.message}</h1>
+      <h1>{errorViewer.message}</h1>
+    </React.Fragment>
+  );
 
   return (
     <Grid item xs={12} md={8}>
