c882ace33a224f150dec7fdd261f496be4ae75a2
cygnu <crucia8@icloud.com>
2021-11-14 20:53:16 +0900

    style: Changed hierarchy for loading and error

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,22 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Grid } from "@mui/material";
-import { PostsContext } from "../contexts/PostsContext";
-import { useViewer } from "../contexts/ViewerContext";
 import { PostsList } from "./PostsList";
 
 export const Main: React.FC = () => {
-  const { loadingPosts, errorPosts } = useContext(PostsContext);
-  const { loadingViewer, errorViewer } = useViewer();
-
-  if (loadingPosts || loadingViewer) return <h1>Loading...</h1>;
-  if (errorPosts || errorViewer)
-    return (
-      <React.Fragment>
-        <h1>{errorPosts?.message}</h1>
-        <h1>{errorViewer?.message}</h1>
-      </React.Fragment>
-    );
-
   return (
     <Grid item xs={12} md={8}>
       <PostsList />
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -2,40 +2,51 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { PostsContext } from "../contexts/PostsContext";
 import { PostContext } from "../contexts/PostContext";
+import { useViewer } from "../contexts/ViewerContext";
 
 export const PostsList: React.FC = () => {
-  const { dataPosts } = useContext(PostsContext);
+  const { loadingPosts, errorPosts, dataPosts } = useContext(PostsContext);
+  const { loadingViewer, errorViewer } = useViewer();
   const { getPost } = useContext(PostContext);
 
   return (
-    <ul>
-      {dataPosts == null ? (
-        <h1>No posts yet.</h1>
-      ) : (
-        dataPosts.allPosts &&
-        dataPosts.allPosts.edges.map((post: any) => (
-          <li key={post.node.id}>
-            <img src={post.node.thumbnail} alt={post.node.thumbnail} />
-            {getPost.id === post.node.id ? (
-              <Link
-                to={`/posts/${post.node.id}`}
-                onClick={async () => {
-                  await getPost({
-                    variables: {
-                      id: post.node.id,
-                    },
-                  });
-                }}
-              >
-                <h1>{post.node.title}</h1>
-              </Link>
-            ) : (
-              <React.Fragment />
-            )}
-            <span>{post.node.updatedAt}</span>
-          </li>
-        ))
+    <React.Fragment>
+      {(loadingPosts || loadingViewer) && <h1>Loading...</h1>}
+      {(errorPosts || errorViewer) && (
+        <React.Fragment>
+          <h1>{errorPosts?.message}</h1>
+          <h1>{errorViewer?.message}</h1>
+        </React.Fragment>
       )}
-    </ul>
+      <ul>
+        {dataPosts == null ? (
+          <h1>No posts yet.</h1>
+        ) : (
+          dataPosts.allPosts &&
+          dataPosts.allPosts.edges.map((post: any) => (
+            <li key={post.node.id}>
+              <img src={post.node.thumbnail} alt={post.node.thumbnail} />
+              {getPost.id === post.node.id ? (
+                <Link
+                  to={`/posts/${post.node.id}`}
+                  onClick={async () => {
+                    await getPost({
+                      variables: {
+                        id: post.node.id,
+                      },
+                    });
+                  }}
+                >
+                  <h1>{post.node.title}</h1>
+                </Link>
+              ) : (
+                <React.Fragment />
+              )}
+              <span>{post.node.updatedAt}</span>
+            </li>
+          ))
+        )}
+      </ul>
+    </React.Fragment>
   );
 };
