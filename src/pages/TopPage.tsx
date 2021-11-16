a824e52a0c689e423c5bfd3be15ecf64f312e0ba
cygnu <crucia8@icloud.com>
2021-11-17 03:51:13 +0900

    refactor: I've rewritten it into another file

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Grid } from "@mui/material";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
 import { Sidebar } from "../components/Sidebar";
+import { Loading } from "../components/Loading";
+import { useViewer } from "../contexts/ViewerContext";
+import { PostsContext } from "../contexts/PostsContext";
 
 export const TopPage: React.FC = () => {
+  const { loadingViewer } = useViewer();
+  const { loadingPosts } = useContext(PostsContext);
+
+  if (loadingViewer || loadingPosts) return <Loading />;
+
   return (
     <React.Fragment>
       <Header />
